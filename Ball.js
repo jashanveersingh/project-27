465c43b3031f005ad43cc079d66ce6802e5dcabf
jashanveersingh <78601252+jashanveersingh@users.noreply.github.com>
2021-08-03 18:40:27 +0530

    Add files via upload

--- Ball.js
+++ Ball.js
@@ -1,26 +1,34 @@
-class Ball{
+class Ball
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0,
+			friction:0,
+			density:0.8
 
-    constructor(x,y,r){
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
 
-        var options={
-            restitution:1
-        }
-        
-            this.body=Bodies.rectangle(this.x,this.y,this.r/2,options)
-            World.add(world,this.body)
-        }
-        display(){
-        var pos=this.body.position;
-        //var angle=this.body.angle;
-        push()
-        translate(pos.x,pos.y)
-        //rotate(angle)
-        ellipseMode(CENTER)
-        fill(255)
-        ellipse(0,0,this.r,this.r)
-        pop()
-        
-        
-        }
-        
-        }
+	}
+	display()
+	{
+			
+			var bobpos=this.body.position;		
+
+			push()
+			translate(bobpos.x, bobpos.y);
+			ellipseMode(RADIUS)
+			
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}
